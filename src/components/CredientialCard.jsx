0cdea2b4fe69543d6973cb8660a547fca1dcb4d0
PeterBYUI <pierre.gavino@gmail.com>
2025-11-16 22:18:39 +0100

    Increased the width of the CredentialCard component for small devices

--- src/components/CredientialCard.jsx
+++ src/components/CredientialCard.jsx
@@ -1,5 +1,5 @@
 export default function CredientialCard({ styling, children }) {
-  let defaultStyling = "w-2/3 mx-auto mt-16 bg-[#e6e6fa] shadow-[0_5px_10px_rgba(0,0,0,.3)] rounded-md ";
+  let defaultStyling = "w-9/10 md:w-3/4 lg:w-2/3 mx-auto mt-16 bg-[#e6e6fa] shadow-[0_5px_10px_rgba(0,0,0,.3)] rounded-md mb-8 ";
   const finalStyling = defaultStyling + styling;
 
   return <div className={finalStyling}>{children}</div>;
