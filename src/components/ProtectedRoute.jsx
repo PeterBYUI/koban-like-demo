b08a3b08888b5fb35de68b02d637a09e4e72260f
PeterBYUI <pierre.gavino@gmail.com>
2025-11-17 15:17:11 +0100

    fix: comment typos

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,11 +3,10 @@ import { useContext } from "react";
 import { Navigate } from "react-router";
 
 export default function ProtectedRoute({ children }) {
-  const { user } = useContext(AuthContext); //takes time... undefined -> user || udefined -> null (if not user is detected)
+  const { user } = useContext(AuthContext); //takes time... undefined -> user || undefined -> null (if no user is detected)
 
   if (user === undefined) {
-    // console.log("Fetching the user...");
-    return <p>Fetching the user...</p>; //add a spiner
+    return <p>Fetching the user...</p>; //add a spinner
   }
   if (user === null) {
     console.log("No user is logged in");
